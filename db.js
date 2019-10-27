215fe3b0d900dfafd40005d35a6c851fcf914bd3
Robert Wise <robcodehub@gmail.com>
2019-10-27 16:56:50 +0700

    updated models, rending nav bar and components

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 
 const Sequelize = require('sequelize');
 
-const {TEXT, STRING, DECIMAL, INTEGER, UUID, UUIDV4} = Sequelize;
+const {TEXT, ARRAY, STRING, DECIMAL, INTEGER, UUID, UUIDV4} = Sequelize;
 
 const conn = new Sequelize(process.env.DATABASE || 'postgres://localhost/teamb-graceshopperdb')
 
@@ -35,6 +35,14 @@ const User = conn.define('user', {
   name: {
     type: STRING
   }
+
+  /* COMMMENTING THIS OUT FOR NOW AS NOT SURE HOW THIS WILL WORK
+  ,
+  cart: {
+    type: ARRAY(INTEGER),
+    defaultValue: []
+  }
+  */
 });
 
 const Product = conn.define('product', {
@@ -61,24 +69,50 @@ const Product = conn.define('product', {
     //STRING FOR NOW BUT COULD BE AN ARRAY IF MULTIPLE PHOTOS
     type: STRING,
     defaultValue: "https://live.staticflickr.com/15/89773667_f91cdd7c11.jpg"
+  },
+  inventory: {
+    type: INTEGER,
+    defaultValue: 1,
+    validate: {
+      min: 0
+    }
   }
-
 
 });
 
+//==============================ORDERS?==============================
+//==============HOW TO HANDLE ORDERS? SEPERATE MODEL OR ARRAY ON USER?
+
+//==============================CART?==============================
+//==============HOW TO HANDLE CART? SEPERATE MODEL OR ARRAY ON USER?
+//==============SHOULD CART BY HANDLED BY STORE AND FRONT END? WILL THIS BE PERSISTENT?
+
 
 //SYNC AND SEED COMING SOON...
-/*
+
 const syncAndSeed = async() => {
   await conn.sync({ force: true}); //THIS NEEDS TO BE REMOVED IN FINAL VERSION
 
+  const users = [
+
+  ]
+
+  const [] = await Promise.all(users.map(user => User.create(user)));
+
+  const products = [
+
+  ]
+
+    const [] = await Promise.all(products.map(product => Product.create(product)));
 
 }
+
 
 module.exports = {
-  syncAndSee,
+  syncAndSeed,
   models: {
-
+    Product,
+    User
   }
 }
-*/
+
--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const path = require('path');
 
 const app = express();
 
-//const db = require('./db');
+const db = require('./db');
+const { models } = require('./db');
+const { User, Product } = models;
 
 const Sequelize = require('sequelize');
 
 
 
+//ISSUES WITH WEBPACK - COMMENTING OUT THE BELOW
 //app.use(express.json());
+//app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
 const port = process.env.PORT || 3000;
 
@@ -18,6 +22,11 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, './index.html'))
 })
 
+app.get('/api/products', (req, res, next) => {
+  Product.findAll()
+  .then(products => res.send(products))
+  .catch(next)
+});
 
 
 
