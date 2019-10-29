1449a9b3dcf4f62ba080df0474826ba173744e2f
Dominique <dominiqueboyer94@yahoo.com>
2019-10-28 20:52:10 -0700

    added add to cart button and created cart page

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const Cart = () => {
-  return (
-    <div>
-      <h1> Shopping Cart coming soon...</h1>
-    </div>
-  )
+
+
+class Cart extends React.Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return (
+      <div>
+        <h1>Your Shopping Cart</h1>
+        <div id='cart'>
+
+
+        </div>
+        <div id='total'>
+
+        </div>
+      </div>
+    );
+  }
 }
+
+
+// const _Cart = () => {
+//   return (
+//     <div>
+//       <h1> Shopping Cart coming soon...</h1>
+//     </div>
+//   )
+// }
+
+
+// const dispatchToProps = (dispatch) => {
+//   return {
+//     delete: async ()=> dispatch(...'coming soon')
+//   }
+// }
+
+// const Cart = connect(null, dispatchToProps)(_Cart)
 
 export default Cart;
