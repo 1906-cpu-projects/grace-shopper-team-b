460e8ff5291f3732dd8782dd1084a00318e73a4b
Robert Wise <robcodehub@gmail.com>
2019-11-06 00:54:47 +0700

    started on stripe functionality

---
+++ src/components/PaymentPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import {StripeProvider, Elements} from 'react-stripe-elements';
+
+import PaymentForm from '../forms/PaymentForm';
+
+
+class PaymentPage extends Component {
+  constuctor() {
+    super()
+  }
+
+  render() {
+    return (
+      <StripeProvider apiKey="pk_test_LxcyyDJDaxKtP8uy5xDO4xHr00zLPYZtPy">
+        <Elements>
+          <PaymentForm />
+        </Elements>
+
+      </StripeProvider>
+
+    )
+  }
+}
+
+export default PaymentPage;
